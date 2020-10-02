a6c72d6ddfa71a72d9f4ef79c45df5c2d8be4e85
SK-website <vnali@tutanota.com>
2020-10-02 18:56:09 +0300

    sidebar ready for dinamic

--- js/likes.js
+++ js/likes.js
@@ -1,19 +1,29 @@
 "use strict"
 
-let likesTotal = document.querySelector('.like-me');
+const likesTotal = document.querySelector('.like-me');
 let hearts = document.querySelectorAll('.fa-thumbs-up');
-let addToCart = document.querySelector('.to-cart');
+const addToCart = document.querySelector('.to-cart');
 let adds = document.querySelectorAll('.fa-shopping-cart');
 let cartTitles = document.querySelectorAll('.cart-title');
+const sidebarOpen = document.querySelector('.fa-shopping-basket');
+const sidebar = document.querySelector('.sidebar');
 
-let heartTitles = document.querySelectorAll('.heart-title')
+let heartTitles = document.querySelectorAll('.heart-title');
 const cartItems = document.querySelector('.cart-items');
 const trash = document.querySelector('.fa-trash-alt');
+const closeBtn = document.querySelector('.close-btn');
 
 
+//open/close sidebar
+sidebarOpen.addEventListener ('click', function () {
+sidebar.classList.toggle('show-sidebar');
+});
 
- /* настройка счетчика лайков + смена вида кнопки, если товар уже был отмечен+ 
- Смена подсказки */  
+closeBtn.addEventListener('click', function (){
+    sidebar.classList.toggle('show-sidebar');
+});
+
+ /* счетчика лайков + смена вида кнопки и подсказки, если товар уже был отмечен*/  
 for (let i=0; i<hearts.length; i++) {
     hearts[i].addEventListener ('click', function (){
         if (hearts[i].classList.contains('added')) {
@@ -26,8 +36,7 @@ for (let i=0; i<hearts.length; i++) {
         hearts[i].classList.toggle('added');
     })
 }
-/* настройка счетчика лайков + смена вида кнопки, если товар уже был отмечен+ 
- Смена подсказки */ 
+/* счетчик товаров в корзине + смена вида кнопки и подсказки, если товар уже в корзине*/ 
 for (let i=0; i<adds.length; i++) {
     adds[i].addEventListener ('click', function (){
     if (adds[i].classList.contains('added')) {
@@ -43,7 +52,7 @@ for (let i=0; i<adds.length; i++) {
 
 cartItems.addEventListener('click', function(event){
         
-    if (event.target.classList.contains('fa-caret-right')) { //Увелич кол-ва товара в корзине
+    if (event.target.classList.contains('fa-caret-right')) { //Увелич кол-ва тов-ра в корзине
         +event.target.previousElementSibling.innerText++;
         //Увеличение субтотал
         event.target.parentNode.parentNode.nextElementSibling.lastChild.
@@ -51,7 +60,7 @@ cartItems.addEventListener('click', function(event){
             parentNode.nextElementSibling.firstChild.nextElementSibling.innerText*
             +event.target.previousElementSibling.innerText) + ')';
                     
-        //Ниже - уменьшение кол-ва товара в корзине+ограничения на уменьшение меньше "0" 
+        //Уменьшение кол-ва товара в корзине+ограничения на уменьшение < "0" 
         } else if (event.target.classList.contains('fa-caret-left') 
         && +event.target.nextElementSibling.innerText>0) {
             +event.target.nextElementSibling.innerText--;
