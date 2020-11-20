67c65eb99e1428142879f529e66327e5f38081cc
SK-website <64695869+SK-website@users.noreply.github.com>
2020-11-20 22:42:24 +0200

    Delete index.php

--- app/Views/contact/index.php
+++
@@ -1,9 +0,0 @@
-
-<div class="container">
-        <div class="row mb-3">
-                <?php require_once VIEWS.'/contact/_formFeedback.php';?>
-                <?php require_once VIEWS.'/contact/_formAddress.php';?>
-
-        </div>
-</div>
-
