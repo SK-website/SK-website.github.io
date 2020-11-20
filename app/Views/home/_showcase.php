5d0362b14e1263bb7eaa64ecc2e971a9517c2a4e
SK-website <64695869+SK-website@users.noreply.github.com>
2020-11-20 22:43:01 +0200

    Delete _showcase.php

--- app/Views/home/_showcase.php
+++
@@ -1,9 +0,0 @@
-<div class="row product">
-    <section>
-            <header>
-                <h3 class="mt-3">НОВЫЕ ПОСТУПЛЕНИЯ</h3>
-            </header>
-            <div class="row showcase">
-            </div>
-    </section>
-</div>
