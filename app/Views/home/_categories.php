a72c0b9f73b079b2d8143414924b1c4a51eac878
SK-website <64695869+SK-website@users.noreply.github.com>
2020-11-20 22:42:47 +0200

    Delete _categories.php

--- app/Views/home/_categories.php
+++
@@ -1,9 +0,0 @@
-<!--Категории new-->
-    <section class="collections" >
-        <header>
-            <h3 class="mt-3">КАТЕГОРИИ</h3>
-        </header>
-        <div class="row categories">
-        </div>
-    </section>
-
